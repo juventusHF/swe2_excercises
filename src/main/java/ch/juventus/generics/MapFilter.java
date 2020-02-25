38d9d4be659067af55e239cd9125712a836e88d8
gitLinda <lkrueger17@gmail.com>
2020-02-25 12:30:07 +0100

    implement generics exercise

---
+++ src/main/java/ch/juventus/generics/MapFilter.java
@@ -0,0 +1,18 @@
+package ch.juventus.generics;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class MapFilter {
+
+    public <K, V> Map<K, V> cleanup(Map<K, V> map, V value) {
+        Iterator<Map.Entry<K, V>> itr = map.entrySet().iterator();
+        while(itr.hasNext()) {
+            Map.Entry<K, V> entry = itr.next();
+            if(entry.getValue() != value) {
+                itr.remove();
+            }
+        }
+        return map;
+    }
+}
---
+++ src/test/java/ch/juventus/generics/MapFilterTest.java
@@ -0,0 +1,27 @@
+package ch.juventus.generics;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapFilterTest {
+
+    private final MapFilter mapFilter = new MapFilter();
+
+    @Test
+    public void testCleanup() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(0, "A");
+        map.put(1, "B");
+        map.put(2, "C");
+        map.put(3, "D");
+        map.put(4, "B");
+
+        Map<Integer, String> filteredMap = mapFilter.cleanup(map, "B");
+
+        assertEquals(2, filteredMap.size());
+    }
+}
