38198ddee6078c0f1208ceeb869e9d9b21daf06d
Linda Krueger <lkrueger17@gmail.com>
2020-09-24 12:20:05 +0200

    refactor map filter

--- src/main/java/ch/juventus/generics/MapFilter.java
+++ src/main/java/ch/juventus/generics/MapFilter.java
@@ -1,18 +1,11 @@
 package ch.juventus.generics;
 
-import java.util.Iterator;
 import java.util.Map;
 
 public class MapFilter {
 
     public <K, V> Map<K, V> cleanup(Map<K, V> map, V value) {
-        Iterator<Map.Entry<K, V>> itr = map.entrySet().iterator();
-        while(itr.hasNext()) {
-            Map.Entry<K, V> entry = itr.next();
-            if(entry.getValue() != value) {
-                itr.remove();
-            }
-        }
+        map.entrySet().removeIf(entry -> !value.equals(entry.getValue()));
         return map;
     }
 }
--- src/test/java/ch/juventus/generics/MapFilterTest.java
+++ src/test/java/ch/juventus/generics/MapFilterTest.java
@@ -2,7 +2,9 @@ package ch.juventus.generics;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,6 +23,20 @@ public class MapFilterTest {
         map.put(4, "B");
 
         Map<Integer, String> filteredMap = mapFilter.cleanup(map, "B");
+
+        assertEquals(2, filteredMap.size());
+    }
+
+    @Test
+    public void testCleanup2() {
+        Map<String, String> map = new HashMap<>();
+        map.put("0", "A");
+        map.put("1", "B");
+        map.put("2", "C");
+        map.put("3", "D");
+        map.put("4", "B");
+
+        Map<String, String> filteredMap = mapFilter.cleanup(map, "B");
 
         assertEquals(2, filteredMap.size());
     }
