0a38357f37c7a384c289d297a3c75c503ad34590
Linda Krueger <lkrueger17@gmail.com>
2020-10-22 18:42:34 +0200

    add file input/output stream exercise

---
+++ src/main/java/ch/juventus/iostreams/ByteStreamExercise.java
@@ -0,0 +1,24 @@
+package ch.juventus.iostreams;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ByteStreamExercise {
+
+    public static void main(String[] args) throws IOException {
+
+        copy("src/main/resources/hello.txt", "src/main/resources/new.txt");
+    }
+
+    private static void copy(String from, String to) throws IOException {
+        FileInputStream inputStream = new FileInputStream(from);
+        FileOutputStream outputStream = new FileOutputStream(to);
+
+        int b;
+        while ((b = inputStream.read()) != -1 ) {
+            outputStream.write(b);
+        }
+        inputStream.close();
+    }
+}
